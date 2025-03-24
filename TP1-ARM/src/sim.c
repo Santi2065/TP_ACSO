ce3c05ae76f925a4b5f2caec5eabe6824d945218
Santiago Groba <santiagogroba@hotmail.com>
2025-03-24 11:28:20 -0300

    Decoficar instrucciones, y switch para correr la que corresponda

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include "shell.h"
 
 void process_instruction()
 {
@@ -8,7 +9,17 @@ void process_instruction()
      * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
      * access memory. 
      * */
-    if (CURRENT_STATE.PC == 0xd4400000){
-        return;
-     }
+    uint32_t instruction = mem_read_32(CURRENT_STATE.PC);
+    uint32_t opcode = (instruction >> 21) & 0x7FF;
+
+    switch (opcode) {
+        case 0x6A2:  // HLT
+            RUN_BIT = FALSE;  // Detener simulación
+            break;
+        default:
+            printf("Instrucción desconocida: %x\n", opcode);
+            break;
+        // Otros casos como ADDS, SUBS, ANDS...
+    }
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 }
